dfbeb24b70b8160e604561bb3629e0ec263d40f0
Gabriel Nordeborn <gabbe.nord@gmail.com>
2019-10-22 18:51:39 +0200

    Oops, missed a few subscription files...

---
+++ src/subscriptionFeed.ts
@@ -0,0 +1,35 @@
+/**
+ * Sets up random updates to the various site statistics, and triggers
+ * the appropriate subscriptions.
+ */
+
+import { siteStatistics } from "./db";
+import { pubsub } from "./subscriptions";
+import { SITE_STATISTICS_UPDATED } from "./subscriptionTopics";
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * max) + min;
+}
+
+function randomizeVisitorCount() {
+  if (Math.random() > 0.25) {
+    siteStatistics.currentVisitorsOnline += randomInt(0, 50);
+  } else {
+    siteStatistics.currentVisitorsOnline -= randomInt(0, 50);
+  }
+
+  pubsub.publish(SITE_STATISTICS_UPDATED, {
+    siteStatisticsUpdated: siteStatistics
+  });
+}
+
+function setupSiteStatisticsRandomizer() {
+  setTimeout(() => {
+    randomizeVisitorCount();
+    setupSiteStatisticsRandomizer();
+  }, randomInt(5000, 10000));
+}
+
+export function setupSubscriptionFeed() {
+  setupSiteStatisticsRandomizer();
+}
