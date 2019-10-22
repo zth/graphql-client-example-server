fec89de840e4fc75f1f5a3ca258730f74b762f21
Gabriel Nordeborn <gabbe.nord@gmail.com>
2019-10-22 19:38:42 +0200

    Add copyPackageJson script to prepareForPublish.

--- prepareForPublish.sh
+++ prepareForPublish.sh
@@ -2,7 +2,7 @@ rm -rf out;
 mkdir -p out/bin;
 yarn;
 yarn build-artifact;
-cp package.json out/;
+node copyPackageJson.js;
 echo "#!/usr/bin/env node" > out/bin/temp;
 cat out/bin/index.js >> out/bin/temp;
 mv -f out/bin/temp out/bin/index.js;
