549ea272a4de0cd8ebca839db05f83cdd196c226
XiNiHa <me@xiniha.dev>
2022-05-19 20:59:01 +0900

    Use envelop-plugin-relay-defer

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { createServer } from '@graphql-yoga/node';
+import relayDeferPlugin from 'envelop-plugin-relay-defer';
 
 import { schema } from './schema';
 import { setupSubscriptionFeed } from './subscriptionFeed';
 
 let server = createServer({
   schema,
-  port: parseInt(process.env.PORT || '4000')
+  port: parseInt(process.env.PORT || '4000'),
+  plugins: [relayDeferPlugin()]
 });
 
 server.start().then(() => {
